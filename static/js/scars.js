4ae7a893dae4ea0db06510549f1dea406be13d1f
Mikey Badr <mfbadr@gmail.com>
2015-03-31 15:15:56 -0400

    every scar shows up now

--- static/js/scars.js
+++ static/js/scars.js
@@ -3,13 +3,24 @@
 $(function(){
   var id = window.location.pathname.replace("/", "");
 
-  var scars_canvas = document.getElementById("scars");
-  scars_canvas.style.width = window.innerWidth + 'px';
-  scars_canvas.style.border = "2px solid black";
-  scars_canvas.style.height = window.innerHeight + 'px';
-  //var scars_context = scars_canvas.getContext("2d");
+  var scars_canvas = document.getElementById('scars');
+  var scars_context = scars_canvas.getContext("2d");
 
-  var scarsFabric = new fabric.Canvas('scars');
+  var scarsFabric = new fabric.Canvas('scars', {
+    backgroundColor: '#000000'
+  });
+
+  //scars_canvas.style.border = "2px solid black";
+  var width = window.innerWidth;
+  var height = window.innerHeight;
+  console.log('width', width);
+  console.log('height', height);
+  scars_canvas.style.width = width + 'px';
+  scars_canvas.style.height = height + 'px';
+  scars_context.canvas.width = width;
+  scars_context.canvas.height = height;
+  scars_canvas.style.border = '2px solid white';
+
 
 
   function getScars(id, cb){
@@ -26,16 +37,32 @@ $(function(){
 
 
   function drawScars(visits){
+    //set up counters, analyze canvas
+    var fabricScars = [],
+        colWidth = width / 11;
+
+    console.log('colwidth', colWidth);
+
+
+    //construct a fabric object based on visit attr
+    var row = 1;
     for(var i = 0; i < visits.length; i++){
-      var rect = new fabric.Rect({
-        left: 10 * visits[i].id,
-        top: 10 * visits[i].id,
+      //swithc on tablet/pc/mobile
+
+      var thisScar = new fabric.Rect({
+        left: colWidth * ((visits[i].id % 10) + 1),
+        top: Math.ceil(visits[i].id / 10) * 25,
         fill: 'red',
         width: 10,
+        angle: 45,
         height: 10,
       })
-      scarsFabric.add(rect);
-      //scars_context.fillRect( 15 , 25 * visits[i].id , 10, 10);
+      fabricScars.push(thisScar);
+    }
+
+    //draw scars
+    for(var i = 0; i < fabricScars.length; i++){
+      scarsFabric.add(fabricScars[i]);
     }
   }
 
